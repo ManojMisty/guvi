7d1e64359a8cf133e0561a406c9375b5dbe9214e
ManojMisty <manojmistymac@gmail.com>
2017-06-05 15:59:03 +0530

    Create o&e.java

---
+++ o&e.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class o&e
+{
+public static void main(String args[])
+{
+Scanner s=new Scanner(System.in);
+System.out.println("Enter the number");
+int n=s.nextInt();
+if(n%2==0)
+System.out.println("even");
+else
+System.out.println("odd");
+}}
