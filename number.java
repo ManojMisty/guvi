0dcf410dca54d490ba62c94390b7643a3fa42324
ManojMisty <manojmistymac@gmail.com>
2017-06-05 15:11:22 +0530

    Update number.java

--- number.java
+++ number.java
@@ -1,1 +1,15 @@
-
+import java.util.*;
+import java.io.*;
+public class number
+{
+  public static void main(String args[])
+  {
+    Scanner s=new Scanner(System.in);
+    int n=s.nextInt();
+    if(n<0)
+      System.out.println("negative");
+    if(n>0)
+      System.out.println("postive");
+    if(n==0)
+      System.out.println("zero");
+    d}}
