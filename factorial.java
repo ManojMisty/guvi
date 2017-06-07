9d7cfaf1782cb48cdd4a807bda7e9465098b74de
ManojMisty <manojmistymac@gmail.com>
2017-06-07 10:33:01 +0530

    Update factorial.java

--- factorial.java
+++ factorial.java
@@ -9,11 +9,13 @@ System.out.println("Enter the number");
 int n=s.nextInt();
 int x=1;
 if(n<0)
+{
   for(int i=1;i<=n;i++)
 {
 x=x*i;
 }
 System.out.println("the factorial of the number is "+x);
+}
 else
 {System.out.println(-1);
 }
