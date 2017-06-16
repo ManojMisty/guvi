9015400d4e04bee1f5705d5cf4229acc2731c3e1
ManojMisty <manojmistymac@gmail.com>
2017-06-16 07:43:54 +0530

    Create string2.java

---
+++ string2.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class string2
+{
+public static void main(String args[])
+{
+
+Scanner s=new Scanner(System.in);
+String s3=s.nextLine();
+String s4[]=s3.split(" ");
+System.out.println(s4[1]+" "+s4[0]);
+}}
