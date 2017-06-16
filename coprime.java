2784924fbbd5a432b6e6e169c54f044f9d4d6a98
ManojMisty <manojmistymac@gmail.com>
2017-06-16 07:51:23 +0530

    Create coprime.java

---
+++ coprime.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+public class coprime
+{
+public static void main(String args[])
+{
+
+Scanner s=new Scanner(System.in);
+System.out.println("enter two prime no");
+int m=s.nextInt();
+int n=s.nextInt();
+if(m%1==0 && n%1==0)
+System.out.println("coprime");
+}}
